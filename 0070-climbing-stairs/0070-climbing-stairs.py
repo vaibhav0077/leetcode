afeed483f252a3477de3ccf4153cb1276b6bbebc
Patel vaibhav <38983632+vaibhav0077@users.noreply.github.com>
2023-02-15 10:08:55 +0530

    Time: 32 ms (64.30%), Space: 13.8 MB (48.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        tab=[0]*45
-        tab[0]=1
-        tab[1]=2
-        for i in range(2,n):
-            tab[i]=tab[i-1]+tab[i-2]
+    
+        dp = [-1 for i in range(n)]
+        def sol(i):
+            if i == n: return 1
+            if i > n : return 0
+            if dp[i] != -1: return dp[i]
+            
+            two = 0
+            if n-i > 1:
+                two = sol(i+2)
+            one = sol(i+1)
+            
+            dp[i] = one + two
+            return dp[i]
         
-        return tab[n-1]
+        sol(0)
+        return dp[0]
+            
