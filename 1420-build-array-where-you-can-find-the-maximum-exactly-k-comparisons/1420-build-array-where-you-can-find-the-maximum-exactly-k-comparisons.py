819cf6acb38f08fe1f55b793094b65beb386815d
Patel vaibhav <38983632+vaibhav0077@users.noreply.github.com>
2023-10-07 10:49:33 +0530

    Time: 96 ms (92.04%), Space: 16.4 MB (95.58%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -1,25 +1,12 @@
 class Solution:
     def numOfArrays(self, n: int, m: int, k: int) -> int:
-        dp = [[[-1 for _ in range(101)] for _ in range(101)] for _ in range(101)]
-        def sol(idx, lenList, lar):
-            if idx == n:
-                if lenList == k:
-                    return 1
-                return 0
-            # print(idx, lenList, lar)
-            if dp[idx][lenList][lar] != -1: return dp[idx][lenList][lar]
-            ans = 0
-            for i in range(1, m+1):
-                if i > lar:
-                    ans += sol(idx+1, lenList+1, i)
-                else:
-                    ans += sol(idx+1, lenList, lar)
-                    
-                ans = ans%(10**9 + 7)
-                
-            dp[idx][lenList][lar] = ans
-                
-            return dp[idx][lenList][lar]
-            
-        return sol(0,0,0)
-        
+        if m < k: return 0
+        dp = [[1] * m] + [[0] * m for _ in range(k - 1)]
+        mod = 10 ** 9 + 7
+        for _ in range(n - 1):
+            for i in range(k - 1, -1, -1):
+                cur = 0
+                for j in range(m):
+                    dp[i][j] = (dp[i][j] * (j + 1) + cur) % mod
+                    if i: cur = (cur + dp[i - 1][j]) % mod
+        return sum(dp[-1]) % mod
