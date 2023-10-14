8d90c699144640d3c7fe3704164db689584700ee
Patel vaibhav <38983632+vaibhav0077@users.noreply.github.com>
2023-10-14 21:48:55 +0530

    Time: 2828 ms (15.92%), Space: 437.7 MB (30.00%) - LeetHub

---
+++ 2742-painting-the-walls/2742-painting-the-walls.py
@@ -0,0 +1,18 @@
+class Solution:
+    def paintWalls(self, cost: List[int], time: List[int]) -> int:
+        dp = {}
+        def sol(i, remain):
+            if remain <= 0:
+                return 0
+            if i == len(cost): return float('inf')
+            if (i, remain) in dp: return dp[(i, remain)]
+            
+            
+            paint = cost[i] + sol(i+1, remain - time[i] -1)
+            skip = sol(i+1, remain)
+            
+            dp[(i, remain)] = min(paint, skip)
+            return dp[(i, remain)]
+        
+        sol(0, len(cost))
+        return dp[(0, len(cost))]
